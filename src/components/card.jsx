5d2f2dbaa627c06000611cf3e7aba360501f3b34
Andrew <marianoandres191200@gmail.com>
2022-01-19 21:46:38 -0300

    setting cards structure

---
+++ src/components/card.jsx
@@ -0,0 +1,11 @@
+import '../styles/card.css';
+
+const Card = ({cardNumber}) =>{
+    return(
+        <div className={`card-main-div card-${cardNumber}`}>
+            
+        </div>
+    )
+}
+
+export default Card;
--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -1,9 +1,24 @@
 import '../styles/mainPage.css';
 
+import Card from './card.jsx'
+
 const mainPage = () =>{
     return(
         <div className="main-div">
+            <div className="cards-container">
+                <Card cardNumber={'1'}/>
+                <Card cardNumber={'2'}/>
+                <div className="card-main-div card-3">
+                    <div className="card-3-info">
+                        <h1>Total cases: 0</h1>
+                    </div>
 
+                    <div className="grid-div">
+                        <div><p>Vaccinated: 0</p></div>
+                        <div><p>Non vaccinated: 0</p></div> 
+                    </div>
+                </div>
+            </div> 
         </div>
     )
 }
